38b5682e282a43333f51918126d1e309a4b7bac0
bryanpizzillo <bryanpizzillo@users.noreply.github.com>
2020-07-01 17:34:02 -0400

    Initial commit - Changed API calls to use new glossary API - Moved all images to be local - Updated links to use www.cancer.gov - Analytics updated to AppMeasurement and pulled in locally. - Removed old injectors per environment. The widget will now work   on acsf-dev, test and prod with the correct analytics suites and   source locations. (i.e. test will get the widget from test)

---
+++ public/js/TermDictionaryWidgetEnglish.js
@@ -0,0 +1,24 @@
+﻿(function() {
+  var site = window.location.hostname;
+  
+  var widgetPath = '/widgets/TermDictionaryWidgetEnglish.htm';
+  var widgetUri = 'https://www.cancer.gov' + widgetPath;
+
+  if (site.indexOf('www-dev-acsf.cancer.gov') !== -1 || site.indexOf('www-test-acsf.cancer.gov') !== -1) {
+    widgetUri = 'https://' + site + widgetPath;
+  } else if (site === 'localhost') {
+    widgetUri = window.location.protocol + '//' + window.location.host + widgetPath;
+  }
+
+  var iframe = document.createElement('IFRAME');
+  iframe.style.width = '100%';
+  iframe.style.height = '460px';
+  iframe.frameBorder= '0';
+  iframe.src = widgetUri + "?site=" + site;
+  iframe.id = 'NCITermDictionaryWidgetContainerEnglish';
+  iframe.title = 'NCI - Dictionary of Cancer Terms';
+  iframe.title = document.URL;
+  iframe.name = document.URL;
+  document.write('<div id="NCITermDictionaryWidgetEnglish"></div>');
+  document.getElementById('NCITermDictionaryWidgetEnglish').appendChild(iframe);
+})();
---
+++ public/js/TermDictionaryWidgetSpanish.js
@@ -0,0 +1,23 @@
+﻿(function() {
+  var site = window.location.hostname;
+  
+  var widgetPath = '/widgets/TermDictionaryWidgetSpanish.htm';
+  var widgetUri = 'https://www.cancer.gov' + widgetPath;
+
+  if (site.indexOf('www-dev-acsf.cancer.gov') !== -1 || site.indexOf('www-test-acsf.cancer.gov') !== -1) {
+    widgetUri = 'https://' + site + widgetPath;
+  } else if (site === 'localhost') {
+    widgetUri = window.location.protocol + '//' + window.location.host + widgetPath;
+  }
+
+  var iframe = document.createElement('IFRAME');
+  iframe.style.width = '100%';
+  iframe.style.height = '475px';
+  iframe.frameBorder= '0';
+  iframe.src = widgetUri + "?site=" + site;
+  iframe.id = 'NCITermDictionaryWidgetContainerSpanish';
+  iframe.title = 'NCI - Diccionario de téminos de cáncer';
+
+  document.write('<div id="NCITermDictionaryWidgetSpanish"></div>');
+  document.getElementById('NCITermDictionaryWidgetSpanish').appendChild(iframe);
+})();
